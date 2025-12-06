8c143f5067df5b81074ad62da0d24ff4b594f238
Mandakini-Patil <mandakinipatil2002@gmail.com>
2025-12-06 14:35:46 +0530

    chore: save local MovieCard and movies.css changes before pulling origin/dev

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -14,7 +14,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
 
   return (
     <div className="card h-100 shadow-sm">
-      <img src={movie.imageUrl} className="card-img-top" alt={movie.title} style={{ height: "300px", objectFit: "cover" }} />
+      <img src={movie.imageUrl} className="card-img-top movie-img" alt={movie.title} />
       <div className="card-body d-flex flex-column">
         <h5 className="card-title">{movie.title}</h5>
         <p className="card-text text-muted small">{movie.description}</p>
