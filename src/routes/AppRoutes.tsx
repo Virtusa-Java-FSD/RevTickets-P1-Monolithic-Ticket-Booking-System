a7be228739bb07529b5262b2570e2441377ea0c6
PALLAKA AMULYA <amulyapallaka30@gmail.com>
2025-12-10 06:54:56 +0530

    Add EventDetail page and booking route to AppRoutes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -9,6 +9,7 @@ import Events from "../pages/Events";
 import Concerts from "../pages/Concerts";
 import Travels from "../pages/Travels";
 import Dashboard from "../pages/Dashboard";
+import Booking from "../pages/Booking";
 import { useAuth } from "../context/AuthContext";
 
 const PrivateRoute = ({ children }: { children: ReactNode }) => {
@@ -25,6 +26,7 @@ const AppRoutes = () => (
     <Route path="/movies" element={<Movies />} />
     <Route path="/movies/:movieId" element={<MovieDetail />} />
     <Route path="/events" element={<Events />} />
+    <Route path="/booking/event/:eventId" element={<Booking />} />
     <Route path="/concerts" element={<Concerts />} />
     <Route path="/travels" element={<Travels />} />
     <Route
